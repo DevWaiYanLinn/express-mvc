d19589d957005abdb32eea6a8ccae8f72e5e10aa
WaiYanLin <waiyanlinn.lion@outlook.com>
2023-06-21 02:33:57 +0630

    rm build

--- src/build/config/hash.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    driver: process.env.HASH_DRIVER || 'bcrypt',
-    bcrypt: {
-        salt: process.env.SALT || 10,
-    }
-};
